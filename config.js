b5e3150e2cbd2585c0f36b0a3064c09e029282a3
shoyadbek97 <Porloqyur@gmail.com>
2026-07-25 19:09:02 +0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+// ============================================================
+// SUPABASE SOZLAMALARI — TAYYOR (to'ldirilgan)
+// ============================================================
+
+const SUPABASE_URL = "https://dfxxgretrznyflbmgucw.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_kWxbevzzD9E7Cxue8g4Idg_c9-ipmsj";
+
+const sb = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
